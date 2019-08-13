85cbc60664c10b2a3c735282c400d17fe9c6c0a8
Fujiharuu <54049669+Fujiharuu@users.noreply.github.com>
2019-08-13 16:07:10 +0800

    Update main.cpp

--- SP1Framework/main.cpp
+++ SP1Framework/main.cpp
@@ -10,7 +10,6 @@ const unsigned int gc_uFrameTime = 1000 / gc_ucFPS;    // time for each frame
 
 //main loop declaration
 void mainLoop( void );
-//xdkkldd//////
 // TODO:
 // Bug in waitUntil. it waits for the time from getElapsedTime to waitUntil, but should be insignificant.
 
